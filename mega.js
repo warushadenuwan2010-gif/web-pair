4bc636d2840cf78ff218d36128afe0a25e6011d0
warushadenuwan2010-gif <warushadenuwan2010@gmail.com>
2025-11-02 12:41:51 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'warushadenuwan110@gmail.com',
+    password: 'Waru2509',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
